17f8ed1c997b8d0fbefdf62f2029394806652152
raven920 <raven@ideashock.net>
2024-08-12 18:28:29 -0500

    Lab1

---
+++ Laboratorios/Laboratorio1/lab1_A.py
@@ -0,0 +1,92 @@
+# Lab B
+# Nombre:
+# Grupo: 
+# Cod Estudiante:
+
+######################################
+#                                    #
+#             Punto #1               #
+#                                    #
+######################################
+
+# Cree un algoritmo que encuentre si un número es par o impar.
+# Calcula el contador de frecuencias y orden de magnitud
+
+# INICIO SOLUCIÓN
+# FIN SOLUCIÓN
+
+######################################
+#                                    #
+#             Punto #2               #
+#                                    #
+######################################
+
+# Cree un algoritmo que encuentre el factorial de un número
+# Calcula el contador de frecuencias y orden de magnitud
+
+# INICIO SOLUCIÓN
+# FIN SOLUCIÓN
+
+######################################
+#                                    #
+#             Punto #3               #
+#                                    #
+######################################
+
+# Cree un algoritmo que encuentre si una matriz es identidad.
+# Calcula el contador de frecuencias y orden de magnitud
+
+# INICIO SOLUCIÓN
+# FIN SOLUCIÓN
+
+######################################
+#                                    #
+#             Punto #4               #
+#                                    #
+######################################
+
+# Cree un algoritmo que divida un número entre otro haciendo restas sucesivas.
+# Calcula el contador de frecuencias y orden de magnitud
+
+# INICIO SOLUCIÓN
+
+# FIN SOLUCIÓN
+
+######################################
+#                                    #
+#             Punto #5               #
+#                                    #
+######################################
+
+# Cree un algoritmo que invierta cadenas de texto
+# Calcula el contador de frecuencias y orden de magnitud
+
+# INICIO SOLUCIÓN
+
+# FIN SOLUCIÓN
+
+######################################
+#                                    #
+#             Punto #6               #
+#                                    #
+######################################
+
+# Cree un algoritmo que multiplique un numero por otro haciendo sumas sucesivas
+# Calcula el contador de frecuencias y orden de magnitud
+
+# INICIO SOLUCIÓN
+
+# FIN SOLUCIÓN
+
+######################################
+#                                    #
+#             Punto #7               #
+#                                    #
+######################################
+
+# Cree un algoritmo que encuentre el logaritmo de un número haciendo divisiones sucesivas
+# Calcula el contador de frecuencias y orden de magnitud
+
+# INICIO SOLUCIÓN
+
+# FIN SOLUCIÓN
---
+++ Laboratorios/Laboratorio1/lab1_B.py
@@ -0,0 +1,91 @@
+# Nombre:
+# Grupo: 
+# Cod Estudiante:
+
+######################################
+#                                    #
+#             Punto #1               #
+#                                    #
+######################################
+
+# Cree un algoritmo que encuentre si un número es par o impar.
+# Calcula el contador de frecuencias y orden de magnitud
+
+# INICIO SOLUCIÓN
+# FIN SOLUCIÓN
+
+######################################
+#                                    #
+#             Punto #2               #
+#                                    #
+######################################
+
+# Cree un algoritmo que encuentre el factorial de un número
+# Calcula el contador de frecuencias y orden de magnitud
+
+# INICIO SOLUCIÓN
+# FIN SOLUCIÓN
+
+######################################
+#                                    #
+#             Punto #3               #
+#                                    #
+######################################
+
+# Cree un algoritmo que encuentre si una matriz es identidad.
+# Calcula el contador de frecuencias y orden de magnitud
+
+# INICIO SOLUCIÓN
+# FIN SOLUCIÓN
+
+######################################
+#                                    #
+#             Punto #4               #
+#                                    #
+######################################
+
+# Cree un algoritmo que divida un número entre otro haciendo restas sucesivas.
+# Calcula el contador de frecuencias y orden de magnitud
+
+# INICIO SOLUCIÓN
+
+# FIN SOLUCIÓN
+
+######################################
+#                                    #
+#             Punto #5               #
+#                                    #
+######################################
+
+# Cree un algoritmo que determine si una cadena de texto es palindroma.
+# Calcula el contador de frecuencias y orden de magnitud
+
+# INICIO SOLUCIÓN
+
+# FIN SOLUCIÓN
+
+######################################
+#                                    #
+#             Punto #6               #
+#                                    #
+######################################
+
+# Cree un algoritmo que encuentre los valores máximo y mínimo de un arreglo.
+# Calcula el contador de frecuencias y orden de magnitud
+
+# INICIO SOLUCIÓN
+
+# FIN SOLUCIÓN
+
+######################################
+#                                    #
+#             Punto #7               #
+#                                    #
+######################################
+
+# Cree un algoritmo que encuentre la potencia de un número haciendo multiplicaciones sucesivas.
+# Calcula el contador de frecuencias y orden de magnitud
+
+# INICIO SOLUCIÓN
+
+# FIN SOLUCIÓN

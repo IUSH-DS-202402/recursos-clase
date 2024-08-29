0a162880e06cd58a090df5093ca84435d81baaed
raven920 <raven@ideashock.net>
2024-08-29 15:22:15 -0500

    Laboratorio #3

---
+++ Laboratorios/Laboratorio3/lab3_A.py
@@ -0,0 +1,66 @@
+# Lab 3 A
+# Nombre:
+# Grupo: 
+# Cod Estudiante:
+
+######################################
+#                                    #
+#             Punto #1               #
+#                                    #
+######################################
+
+# Cree un método llamado partir(...) dentro de la clase Cola que reciba un parámetro 'n', desencole 'n' elementos y retorne una nueva cola con los elementos que fueron desencolados. Incluya todo el código de la declaración de las clase Cola y un ejemplo del funcionamiento del método.
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
+
+######################################
+#                                    #
+#             Punto #2               #
+#                                    #
+######################################
+
+# Cree un método llamado encolaAlPrincipio(...) dentro de la clase Cola que reciba un dato y lo encole al inicio de la lista de tal forma que sea el siguiente elemento que va a salir al desencolar. Incluya todo el código de la declaración de la clase Cola y un ejemplo del funcionamiento del método.
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
+
+
+######################################
+#                                    #
+#             Punto #3               #
+#                                    #
+######################################
+
+# Realice los cambios requeridos a la clase NodoPrioridad y al método encolar(...) de la clase ColaPrioridad de tal forma que las prioridades en los nodos puedan ser las siguientes: "CRITICAL", "URGENT", "PRIORITY", "NORMAL", "IRRELEVANT" (en ese orden). Incluya todo el código de la declaración de las clases NodoPrioridad, ColaPrioridad y un ejemplo del funcionamiento del método.
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
+
+######################################
+#                                    #
+#             Punto #4               #
+#                                    #
+######################################
+
+# Cree una función llamada main donde simule una aplicación básica de compras y despacho de productos. 
+# - Al iniciar el programa deberá pedirle al usuario que seleccione una de las siguientes opciones:
+#   - Comprar producto: Si el usuario selecciona esta opción se le dará la oportunidad de ingresar su nombre y un producto a comprar. Esta info se debe agregar a la cola
+#   - Validar siguiente producto: mostrará la info del siguiente  nombre y el producto a despachar sin desencolarlo.
+#   - Despachar producto: Si el usuario selecciona esta opción se desencolará un elemento y se mostrará la info del nombre y el producto a despachar.
+#   - Salir del programa: Sale del programa
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
+
+
+######################################
+#                                    #
+#             Punto #5               #
+#                                    #
+######################################
+
+# Cree un método llamado invertirPila(...) dentro de la clase Pila que invierta el orden de los elementos en la pila. Incluya todo el código de la declaración de la clase Pila y un ejemplo del funcionamiento del método.
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
---
+++ Laboratorios/Laboratorio3/lab3_B.py
@@ -0,0 +1,66 @@
+# Lab 3 B
+# Nombre:
+# Grupo: 
+# Cod Estudiante:
+
+######################################
+#                                    #
+#             Punto #1               #
+#                                    #
+######################################
+
+# Cree un método llamado partir(...) dentro de la clase Cola que reciba un parámetro 'n', desencole 'n' elementos y retorne una nueva cola con los elementos que fueron desencolados. Incluya todo el código de la declaración de las clase Cola y un ejemplo del funcionamiento del método.
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
+
+######################################
+#                                    #
+#             Punto #2               #
+#                                    #
+######################################
+
+# Cree un método llamado encolaAlPrincipio(...) dentro de la clase Cola que reciba un dato y lo encole al inicio de la lista de tal forma que sea el siguiente elemento que va a salir al desencolar. Incluya todo el código de la declaración de la clase Cola y un ejemplo del funcionamiento del método.
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
+
+
+######################################
+#                                    #
+#             Punto #3               #
+#                                    #
+######################################
+
+#Cree un método llamado intercalar(...) dentro de la clase Cola que reciba otra cola como parámetro y mezcle los elementos de ambas colas de tal manera que los elementos de la cola actual y la cola pasada como parámetro se alternen. Incluya todo el código de la declaración de la clase Cola y un ejemplo del funcionamiento del método.
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
+
+######################################
+#                                    #
+#             Punto #4               #
+#                                    #
+######################################
+
+# Cree una función llamada 'main' donde simule una aplicación básica de intercambio de libros 
+# - Al iniciar el programa deberá pedirle al usuario que seleccione una de las siguientes opciones:
+#   - Poner libro: Si el usuario selecciona esta opción se le dará la oportunidad de ingresar su nombre y el nombre de un libro que va a poner para intercambio
+#   - Validar siguiente producto: mostrará la info del nombre del anterior dueño y el libro que está esperando para salir pero sin sacarlo.
+#   - Despachar producto: Si el usuario selecciona esta opción se sacará un elemento y se mostrará la info del nombre del dueño anterior y el nombre del libro. Considere que siempre el libro que fue ingresado más recientemente es el primero que tiene que salir.
+#   - Salir del programa: Sale del programa
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
+
+
+######################################
+#                                    #
+#             Punto #5               #
+#                                    #
+######################################
+
+# Modifique la clase Pila e incluya un método llamado obtenerMinimo() que retorne el elemento mínimo de la pila en *tiempo constante*. Incluya todo el código de la declaración de la clase Pila y un ejemplo del funcionamiento del método.
+
+# INICIO_SOLUCION
+# FIN_SOLUCION
